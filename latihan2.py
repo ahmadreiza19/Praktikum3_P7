d5e0d005956446ef0f9eed4474b3703be4e085e8
Ahmad Reiza <ahmadreiza0r@gmail.com>
2020-11-09 00:10:35 -0800

    update

--- latihan2.py
+++ latihan2.py
@@ -1,9 +1,11 @@
 xangka=0
+
 while true:
     xbilangan = xbilangan):
     if (xangka < xbilangan):
         xangka=xbilangan
     if (xbilangan == 0)
         break
+    
 print("Bilangan terbesar adalah : ",xangka)
 print()
